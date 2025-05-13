60d51bc8ae451d94088036df2ec475e7fdee3d6b
ShahinMammadli2 <032290154@ogr.uludag.edu.tr>
2025-05-14 01:35:22 +0300

    linear sampling upgrade

--- Sandbox/blur.fs
+++ Sandbox/blur.fs
@@ -6,26 +6,31 @@ in vec2 TexCoords;
 uniform sampler2D image;
 
 uniform bool horizontal;
-uniform float weight[5] = float[](0.227027, 0.1945946, 0.1216216, 0.054054, 0.016216);
+uniform float weight[3] = float[](0.2270270270, 0.3162162162, 0.0702702703);
+uniform float offset[3] = float[](
+    0.0,
+    (1.0*0.1945945946 + 2.0*0.1216216216) / (0.1945945946 + 0.1216216216),  // ? 1.3846153846
+    (3.0*0.0540540541 + 4.0*0.0162162162) / (0.0540540541 + 0.0162162162)   // ? 3.2307692308
+);
 
 void main()
 {
-    vec2 tex_offset = 1.0 / textureSize(image,0); // gets the size of single texel
+    vec2 tex_offset = 1.0 / textureSize(image, 0);
     vec3 result = texture(image, TexCoords).rgb * weight[0];
     if(horizontal)
     {
-      for(int i = 1; i < 5; i++)
+      for(int i = 1; i < 3; i++)
       {
-         result += texture(image, TexCoords + vec2(tex_offset.x * i, 0.0)).rgb * weight[i];
-         result += texture(image, TexCoords - vec2(tex_offset.x * i, 0.0)).rgb * weight[i];
+         result += texture(image, TexCoords + vec2(tex_offset.x * offset[i], 0.0)).rgb * weight[i];
+         result += texture(image, TexCoords - vec2(tex_offset.x * offset[i], 0.0)).rgb * weight[i];
       }
     }
     else
     {
-      for(int i = 1; i < 5; i++)
+      for(int i = 1; i < 3; i++)
        {
-          result += texture(image, TexCoords + vec2(0.0, tex_offset.y * i)).rgb * weight[i];
-          result += texture(image, TexCoords - vec2(0.0, tex_offset.y * i)).rgb * weight[i];
+          result += texture(image, TexCoords + vec2(0.0, tex_offset.y * offset[i])).rgb * weight[i];
+          result += texture(image, TexCoords - vec2(0.0, tex_offset.y * offset[i])).rgb * weight[i];
        }
     }
     FragColor = vec4(result, 1.0);
